377a84b540e7d574311e9019133693799faeef66
kgoransika <kgoransika@students.nsbm.ac.lk>
2023-02-13 14:13:07 +0530

    -React initialize

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">
+          HELLA <span className="titleDigital">DIGITAL</span>
+        </a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
