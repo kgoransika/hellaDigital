e98032fe275330b61b4488915370ef87afaef2fa
kgoransika <kgoransika@students.nsbm.ac.lk>
2023-05-19 14:08:25 +0530

    -Order model created

---
+++ model/Order.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const OrderSchema = new Schema({
+  orderName: {
+    type: String,
+    ref: 'User',
+    required: true,
+  },
+  orderType: {
+    type: String,
+    required: true,
+  },
+  orderDetails: {
+    type: String,
+    required: true,
+  },
+  orderTotal: {
+    type: Number,
+    required: true,
+  },
+  orderStatus: {
+    type: String,
+    required: true,
+  },
+  sellerName: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model.Orders || mongoose.model('order', OrderSchema);
